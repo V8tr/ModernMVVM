3a218e3b8dc5bc61dd0f95a25966de36ec391c2a
V8tr <ne0_n@mail.ru>
2020-03-13 17:53:27 +0200

    WIP

---
+++ ModernMVVM/Spinner.swift
@@ -0,0 +1,25 @@
+//
+//  Spinner.swift
+//  ModernMVVM
+//
+//  Created by Vadym Bulavin on 3/12/20.
+//  Copyright © 2020 Vadym Bulavin. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct Spinner: UIViewRepresentable {
+    let isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let spinner = UIActivityIndicatorView(style: style)
+        spinner.hidesWhenStopped = true
+        return spinner
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
